2986979de964526bd4201e32fe41b8f28454c9ef
Simon Strobel <simon@codebrew.de>
2020-03-23 17:01:03 +0100

    [LIEFEREMMA] Created services and pages folders and restructured existing pages

--- emmaliefert/src/app/app-routing.module.ts
+++ emmaliefert/src/app/app-routing.module.ts
@@ -2,50 +2,50 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)},
+  { path: '', loadChildren: () => import('./pages/index/index.module').then( m => m.IndexPageModule)},
   {
     path: 'order',
-    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
+    loadChildren: () => import('./pages/order/order.module').then( m => m.OrderPageModule)
   },
   {
     path: 'ordersummary',
-    loadChildren: () => import('./ordersummary/ordersummary.module').then( m => m.OrdersummaryPageModule)
+    loadChildren: () => import('./pages/ordersummary/ordersummary.module').then( m => m.OrdersummaryPageModule)
   },
   {
     path: 'trackorder',
-    loadChildren: () => import('./trackorder/trackorder.module').then( m => m.TrackorderPageModule)
+    loadChildren: () => import('./pages/trackorder/trackorder.module').then( m => m.TrackorderPageModule)
   },
   {
     path: 'successmodal',
-    loadChildren: () => import('./successmodal/successmodal.module').then( m => m.SuccessmodalPageModule)
+    loadChildren: () => import('./pages/successmodal/successmodal.module').then( m => m.SuccessmodalPageModule)
   },
   {
     path: 'driver-overview',
-    loadChildren: () => import('./driver-overview/driver-overview.module').then( m => m.DriverOverviewPageModule)
+    loadChildren: () => import('./pages/driver-overview/driver-overview.module').then( m => m.DriverOverviewPageModule)
   },
   {
     path: 'driver-overview',
-    loadChildren: () => import('./driver-overview/driver-overview.module').then( m => m.DriverOverviewPageModule)
+    loadChildren: () => import('./pages/driver-overview/driver-overview.module').then( m => m.DriverOverviewPageModule)
   },
   {
     path: 'route-overview',
-    loadChildren: () => import('./route-overview/route-overview.module').then( m => m.RouteOverviewPageModule)
+    loadChildren: () => import('./pages/route-overview/route-overview.module').then( m => m.RouteOverviewPageModule)
   },
   {
     path: 'driver-delivery',
-    loadChildren: () => import('./driver-delivery/driver-delivery.module').then( m => m.DriverDeliveryPageModule)
+    loadChildren: () => import('./pages/driver-delivery/driver-delivery.module').then( m => m.DriverDeliveryPageModule)
   },
   {
     path: 'index',
-    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
+    loadChildren: () => import('./pages/index/index.module').then( m => m.IndexPageModule)
   },
   {
     path: 'shopcode',
-    loadChildren: () => import('./shopcode/shopcode.module').then( m => m.ShopcodePageModule)
+    loadChildren: () => import('./pages/shopcode/shopcode.module').then( m => m.ShopcodePageModule)
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
